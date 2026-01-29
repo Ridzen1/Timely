712ca318271815e19e08f94cd3da75b5c2f8acb4
Math54500 <Maths54500z@gmail.com>
2026-01-29 19:43:09 +0100

    ajout de connerie

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Profil from '../views/Profil.vue'
+import Projet from '../views/Projet.vue'
+import Activite from '../views/Activite.vue'
+import Connexion from '../views/Connexion.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/profil',
+      name: 'profil',
+      component: Profil,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/projet',
+      name: 'projet',
+      component: Projet,
+    },
+    {
+      path: '/activite',
+      name: 'activite',
+      component: Activite,
+    },
+    {
+      path: '/connexion',
+      name: 'connexion',
+      component: Connexion,
     },
   ],
 })
